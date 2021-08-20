3e48955e76abbf4135490422bd24dace6bb52712
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2021-08-20 10:52:50 +0100

    Integrate the blog app

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,6 +77,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "blog_with_cookie_cutter.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "blog_with_cookie_cutter.blog.apps.BlogConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
