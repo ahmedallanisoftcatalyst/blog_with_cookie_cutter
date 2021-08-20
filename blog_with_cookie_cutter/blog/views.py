271e4af938c148b21524dc1246eb7ba3b1841ac1
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2021-08-20 13:13:48 +0100

    PostList view

--- blog_with_cookie_cutter/blog/views.py
+++ blog_with_cookie_cutter/blog/views.py
@@ -2,10 +2,15 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
-# Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+from django.views import generic
+
+class PostList(generic.ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
